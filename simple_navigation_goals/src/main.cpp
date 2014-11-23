a51ff161752df88cfba2443bfc76b166bcfbf340
mustafasezer <m.sezer@outlook.com>
2014-11-23 18:46:41 +0100

    Automatic goal reading bug fixed and stabilized package

--- simple_navigation_goals/src/main.cpp
+++ simple_navigation_goals/src/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** argv){
 
     int num=0;
 
-    while(num<19){
+    while(!file.eof()){
         getline(file,s_line);
         line = s_line.c_str();
         if(line.contains("position")){
